52fef64f565e68a5b4c072237e5dab91565132ff
Harry Reeder <harryreeder@hutchinsonnetworks.com>
2018-02-13 10:59:30 +0000

    Add relogin functionality for sensor

---
+++ actions/tenant_create.py
@@ -0,0 +1,18 @@
+from acitoolkit import acitoolkit as aci
+from st2reactor.sensor.base import Action
+
+
+class TenantCreate(Action):
+    def run(self, cluster_name, name, description=None):
+        config = self.config
+        cluster = [cluster for cluster in config['clusters'] if cluster['name'] == cluster_name][0]
+        session = aci.Session(cluster['apics'][0], cluster.get('username', config.get('username')), cluster.get('password', config.get('password')))
+        session.login()
+
+        tenant = aci.Tenant(name)
+        
+        if description:
+            tenant.desc = description
+        
+        tenant.push_to_apic(session)
+        return True
