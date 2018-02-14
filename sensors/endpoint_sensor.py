2a5e5552344785cdf9edef4772b1d7aa77651aeb
Harry Reeder <harryreeder@hutchinsonnetworks.com>
2018-02-14 15:43:33 +0000

    Add basic Endpoint Sensor

---
+++ sensors/endpoint_sensor.py
@@ -0,0 +1,60 @@
+import datetime
+
+from acitoolkit import acitoolkit as aci
+from _base_aci_sensor import ACISensor
+
+
+class EndpointSensor(ACISensor):
+    def setup(self):
+        self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+        super(EndpointSensor, self).setup()
+
+        for session in self.aci_sessions.values():
+            aci.Endpoint.subscribe(session, only_new=True)
+    
+    def poll(self):
+        super(EndpointSensor, self).poll()
+        for cluster_name, session in self.aci_sessions.items():
+            if aci.Endpoint.has_events(session):
+                endpoint = aci.Endpoint.get_event(session)
+
+                trigger = "aci."
+
+                if endpoint.is_deleted():
+                    trigger += "endpoint_deleted"
+                else:
+                    trigger += "endpoint_updated"
+
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={
+                        "cluster": cluster_name,
+                        "ip": endpoint.ip,
+                        "mac": endpoint.mac,
+                        "dn": endpoint.dn,
+                        "epg": endpoint.get_parent().name,
+                        "tenant": endpoint.get_parent().get_parent().get_parent().name
+                    }
+                )
+
+                self._logger.info("Dispatching trigger {}:".format(trigger))
+                self._logger.info({
+                    "cluster": cluster_name,
+                    "ip": endpoint.ip,
+                    "mac": endpoint.mac,
+                    "dn": endpoint.dn,
+                    "epg": endpoint.get_parent().name,
+                    "tenant": endpoint.get_parent().get_parent().get_parent().name
+                })
+
+    def cleanup(self):
+        pass
+
+    def add_trigger(self, trigger):
+        pass
+
+    def update_trigger(self, trigger):
+        pass
+
+    def remove_trigger(self, trigger):
+        pass
