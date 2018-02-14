6a0aea39b862ebb0af26d00f40c62be963303eb4
Harry Reeder <harryreeder@hutchinsonnetworks.com>
2018-02-14 14:45:21 +0000

    Add first form of an IP Endpoint

---
+++ sensors/ipendpoint_sensor.py
@@ -0,0 +1,50 @@
+import datetime
+
+from acitoolkit import acitoolkit as aci
+from _base_aci_sensor import ACISensor
+
+
+class IPEndpointSensor(ACISensor):
+    def setup(self):
+        self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+        super(IPEndpointSensor, self).setup()
+
+        for session in self.aci_sessions.values():
+            aci.IPEndpoint.subscribe(session, only_new=True)
+    
+    def poll(self):
+        super(IPEndpointSensor, self).poll()
+        for cluster_name, session in self.aci_sessions.items():
+            if aci.IPEndpoint.has_events(session):
+                endpoint = aci.IPEndpoint.get_event(session)
+
+                trigger = "aci."
+
+                if endpoint.is_deleted():
+                    trigger += "ipendpoint_deleted"
+                else:
+                    trigger += "ipendpoint_updated"
+
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={
+                        "cluster": cluster_name,
+                        "ip": endpoint.ip,
+                        "mac": endpoint.mac,
+                        "dn": endpoint.dn,
+                        "epg": endpoint.get_parent().name,
+                        "tenant": endpoint.get_parent().get_parent().get_parent().name
+                    }
+                )
+
+    def cleanup(self):
+        pass
+
+    def add_trigger(self, trigger):
+        pass
+
+    def update_trigger(self, trigger):
+        pass
+
+    def remove_trigger(self, trigger):
+        pass
