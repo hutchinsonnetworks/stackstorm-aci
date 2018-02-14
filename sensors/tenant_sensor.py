16aefa7642d944eef72b42dda4b32eac0729a402
Harry Reeder <harryreeder@hutchinsonnetworks.com>
2018-02-14 11:44:44 +0000

    Tuning refresh

--- sensors/tenant_sensor.py
+++ sensors/tenant_sensor.py
@@ -36,8 +36,9 @@ class TenantSensor(ACISensor):
                 )
         
         time_since_last_refresh = datetime.datetime.utcnow() - self._last_refresh
-        if time_since_last_refresh > datetime.timedelta(minutes=5): # 5 minutes for dev just to make sure this is being called
+        if time_since_last_refresh > datetime.timedelta(seconds=240):
             # ACI Tokens expire every 24 hours, we're going to refresh ours every 12 just to be safe
+            # This could actually be incorrect... Some documentation points to every 300 seconds
             self._logger.info("Refreshing APIC Session(s)")
             self._last_refresh = datetime.datetime.utcnow()
             for session in self.aci_sessions.values():
