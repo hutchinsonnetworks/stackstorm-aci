b59c0e04308062bd0ec3bdf8233e11385178a724
Harry Reeder <harryreeder@hutchinsonnetworks.com>
2018-02-14 12:40:24 +0000

    Move common elements into the base ACISensor class

--- sensors/_base_aci_sensor.py
+++ sensors/_base_aci_sensor.py
@@ -1,8 +1,14 @@
+import datetime
+
 from acitoolkit import acitoolkit as aci
 from st2reactor.sensor.base import PollingSensor
 
 
 class ACISensor(PollingSensor):
+    def setup(self):
+        self._last_refresh = datetime.datetime.utcnow()
+        self._setup_sessions()
+
     def _setup_sessions(self):
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._logger.info("Setting up Sessions")
@@ -19,3 +25,13 @@ class ACISensor(PollingSensor):
                 cluster.get('password', config.get('password'))
             )
             self.aci_sessions[name].login()
+
+    def poll(self):
+        time_since_last_refresh = datetime.datetime.utcnow() - self._last_refresh
+        if time_since_last_refresh > datetime.timedelta(seconds=240):
+            # ACI Tokens expire every 24 hours, we're going to refresh ours every 12 just to be safe
+            # This could actually be incorrect... Some documentation points to every 300 seconds
+            self._logger.info("Refreshing APIC Session(s)")
+            self._last_refresh = datetime.datetime.utcnow()
+            for session in self.aci_sessions.values():
+                session.refresh_login()
--- sensors/tenant_sensor.py
+++ sensors/tenant_sensor.py
@@ -7,13 +7,13 @@ from _base_aci_sensor import ACISensor
 class TenantSensor(ACISensor):
     def setup(self):
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
-        self._setup_sessions()
-        self._last_refresh = datetime.datetime.utcnow()
+        super(TenantSensor, self).setup()
 
         for session in self.aci_sessions.values():
             aci.Tenant.subscribe(session, only_new=True)
     
     def poll(self):
+        super(TenantSensor, self).poll()
         for cluster_name, session in self.aci_sessions.items():
             if aci.Tenant.has_events(session):
                 tenant = aci.Tenant.get_event(session)
@@ -34,15 +34,6 @@ class TenantSensor(ACISensor):
                         "description": tenant.descr
                     }
                 )
-        
-        time_since_last_refresh = datetime.datetime.utcnow() - self._last_refresh
-        if time_since_last_refresh > datetime.timedelta(seconds=240):
-            # ACI Tokens expire every 24 hours, we're going to refresh ours every 12 just to be safe
-            # This could actually be incorrect... Some documentation points to every 300 seconds
-            self._logger.info("Refreshing APIC Session(s)")
-            self._last_refresh = datetime.datetime.utcnow()
-            for session in self.aci_sessions.values():
-                session.refresh_login()
 
     def cleanup(self):
         pass
